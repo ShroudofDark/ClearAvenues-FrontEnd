489c3c3cb46c83fdb4c48c15fd4807383fdc292b
AfnanSa002 <99747471+AfnanSa002@users.noreply.github.com>
2023-02-22 12:48:33 -0500

    analysis_screen.dart notification_screen.dart view_organization.dart

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,4 +1,4 @@
-import 'package:clear_avenues/widgets/my_scaffold.dart';
+
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -11,7 +11,67 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
-    // Build the register screen here
-    return const MyScaffold(body: Placeholder());
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text('Register'),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                   const Text(
+                'Create an account',
+                style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+            ),
+            ),
+                  const SizedBox(height: 16.0),
+                  TextFormField(
+                  decoration: const InputDecoration(
+                  hintText: 'Enter your name',
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+            ),
+            ),
+                  const SizedBox(height: 16.0),
+                  TextFormField(
+                  decoration: const InputDecoration(
+                  hintText: 'Enter your email address',
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+            ),
+            ),
+                  const SizedBox(height: 16.0),
+                  TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                  hintText: 'Enter your password',
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+            ),
+            ),
+
+                  const SizedBox(height: 16.0),
+                  TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                  hintText: 'Confirm your password',
+                  labelText: 'Confirm Password',
+                  border: OutlineInputBorder(),
+            ),
+            ),
+
+                  const SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Register'),
+                   ),
+            ],
+            ),
+        ),
+    );
   }
 }
+
