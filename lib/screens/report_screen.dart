283212376022b0400423a9897aa4ff0a41c6e376
Jacob McFadden <jmcfa001@odu.edu>
2023-04-08 00:23:37 -0400

    Prepped Report Submission Button
    Button logic for submit is there now. Won't let you submit unless at minimum you select an unsafe condition type (same goes for coordinates, but there should be coordinates. Though I suppose I should still test for it.) Needs to be updated with proper URL information and tested when backend is fixed up.

--- lib/screens/report_screen.dart
+++ lib/screens/report_screen.dart
@@ -1,12 +1,16 @@
 import 'dart:io';
-
+import 'dart:typed_data';
+import 'dart:convert';
 import 'package:clear_avenues/widgets/my_scaffold.dart';
+import 'package:clear_avenues/utility/http_assist.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 ImagePicker picker = ImagePicker();
+TextEditingController description = TextEditingController();
 
 /// Hacky way to get image list to update by changing this value and having the image
 /// list built inside a value listen builder. Not 100% if the reason this is working is due
@@ -16,11 +20,6 @@ final cheatUpdate = ValueNotifier<int>(0);
 
 //Dismiss dialogue box help
 BuildContext? dcontext;
-dismissDialog() {
-  if(dcontext != null){
-    Navigator.of(dcontext!).pop();
-  }
-}
 
 class ReportScreen extends StatefulWidget {
   final LatLng coordinates;
@@ -44,20 +43,20 @@ class _ReportScreenState extends State<ReportScreen> {
 
   //Allows for Images and Other Customization
   List<UnsafeCondition> conditions = [
-    UnsafeCondition("Debris", "debris",
-        Image.asset("assets/images/Debris64.png")),
-    UnsafeCondition("Flooding", "flooding",
-        Image.asset("assets/images/TrafficCone64.png")),
+    UnsafeCondition(
+        "Debris", "debris", Image.asset("assets/images/Debris64.png")),
+    UnsafeCondition(
+        "Flooding", "flooding", Image.asset("assets/images/TrafficCone64.png")),
     UnsafeCondition("Missing Sign", "missing_signage",
         Image.asset("assets/images/MissingSign64.png")),
-    UnsafeCondition("Pothole", "pothole",
-        Image.asset("assets/images/Pothole64.png")),
+    UnsafeCondition(
+        "Pothole", "pothole", Image.asset("assets/images/Pothole64.png")),
     UnsafeCondition("Obstructed Sign", "obstructed_sign",
         Image.asset("assets/images/ObstructedSign64.png")),
     UnsafeCondition("Vehicular Related", "vehicle_accident",
         Image.asset("assets/images/TrafficCone64.png")),
-    UnsafeCondition("Other", "other",
-        Image.asset("assets/images/TrafficCone64.png")),
+    UnsafeCondition(
+        "Other", "other", Image.asset("assets/images/TrafficCone64.png")),
   ];
 
   List<XFile> imageFileList = []; //For multi-images + gallery
@@ -68,22 +67,17 @@ class _ReportScreenState extends State<ReportScreen> {
   //Function modified from:
   // https://medium.com/@fernnandoptr/how-to-get-users-current-location-address-in-flutter-geolocator-geocoding-be563ad6f66a
   Future<void> _getAddressFromCoords() async {
-    await placemarkFromCoordinates(widget.coordinates.latitude, widget.coordinates.longitude)
+    await placemarkFromCoordinates(
+            widget.coordinates.latitude, widget.coordinates.longitude)
         .then((List<Placemark> placemarks) {
-          Placemark place = placemarks[0];
-          setState(() {
-            _address =
+      Placemark place = placemarks[0];
+      setState(() {
+        _address =
             '${place.street}, ${place.locality}, ${place.administrativeArea} ${place.postalCode}';
-          });
-        });
+      });
+    });
   }
 
-  // static const LatLng initialLocation = const LatLng(36.8855, -76.3058);
-
-  // static LatLng loc = LatLng(1.0,1.0);
-  // static LatLng? loc = passed_location;
-  // static String s = "${loc?.longitude} ${loc?.latitude}";
-  
   @override
   void initState() {
     super.initState();
@@ -106,35 +100,37 @@ class _ReportScreenState extends State<ReportScreen> {
                       border: Border.all(width: 3.0, color: Colors.green),
                     ),
                     child: GoogleMap(
-                        initialCameraPosition: CameraPosition(
-                            target: widget.coordinates,
-                            zoom: 20.0, tilt: 0, bearing: 0
+                      initialCameraPosition: CameraPosition(
+                          target: widget.coordinates,
+                          zoom: 20.0,
+                          tilt: 0,
+                          bearing: 0),
+                      myLocationButtonEnabled: false,
+                      zoomControlsEnabled: false,
+
+                      //disables movement of map
+                      zoomGesturesEnabled: false,
+                      scrollGesturesEnabled: false,
+                      tiltGesturesEnabled: false,
+                      rotateGesturesEnabled: false,
+
+                      markers: {
+                        Marker(
+                          markerId: const MarkerId("location"),
+                          draggable: false,
+                          position: widget.coordinates,
+                          infoWindow:
+                              const InfoWindow(title: "location of issue"),
                         ),
-                        myLocationButtonEnabled: false,
-                        zoomControlsEnabled: false,
-
-                        //disables movement of map
-                        zoomGesturesEnabled: false,
-                        scrollGesturesEnabled: false,
-                        tiltGesturesEnabled: false,
-                        rotateGesturesEnabled: false,
-
-                        markers: {
-                          Marker(
-                            markerId: const MarkerId("location"),
-                            draggable: false,
-                            position: widget.coordinates,
-                            infoWindow: const InfoWindow(title: "location of issue"),
-                          ),
-                        },
-                        onMapCreated: (GoogleMapController controller) {
-                          controller.showMarkerInfoWindow(
-                            const MarkerId("location"));
-                        },
+                      },
+                      onMapCreated: (GoogleMapController controller) {
+                        controller
+                            .showMarkerInfoWindow(const MarkerId("location"));
+                      },
                     ),
                   ),
                 ),
-                Container (
+                Container(
                   padding: const EdgeInsets.all(16.0),
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
@@ -223,24 +219,27 @@ class _ReportScreenState extends State<ReportScreen> {
               }).toList(),
             ),
             Container(
-              padding: const EdgeInsets.symmetric(vertical: 2.0,horizontal: 16.0),
+              padding:
+                  const EdgeInsets.symmetric(vertical: 2.0, horizontal: 16.0),
               decoration: const BoxDecoration(
                 border: Border(top: BorderSide(width: 4.0, color: Colors.cyan)),
               ),
-              child: const TextField(
-                style: TextStyle(
+              child: TextField(
+                controller: description,
+                style: const TextStyle(
                   color: Colors.black,
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
                 ),
-                decoration: InputDecoration(labelText: "Enter Description"),
+                decoration:
+                    const InputDecoration(labelText: "Enter Description"),
                 maxLines: 5,
               ),
             ),
             //Upload image box
             ElevatedButton(
               onPressed: () => _onPressUpload(context, imageFileList),
-              child: Row (
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: const [
                   Icon(
@@ -255,49 +254,46 @@ class _ReportScreenState extends State<ReportScreen> {
             ),
             //Image display
             ValueListenableBuilder(
-              valueListenable: cheatUpdate,
-              builder: (context, value, widget) {
-                if (imageFileList.isNotEmpty) {
-                  return SizedBox(
-                      height: 150,
-                      child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: imageFileList.length,
-                          itemBuilder: (context, index) {
-                            return SizedBox(
-                              height: 150,
-                              //Row here in order to add a more controlled version of spacing between the images
-                              child: Row(
-                                children: [
-                                  Image.file(File(imageFileList[index].path)),
-                                  const SizedBox(
-                                    width: 5,
-                                  ),
-                                ],
-                              ),
-                            );
-                          }
-                      )
-                  );
-                }
-                else {
-                  return const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                      "Images Are Optional",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                valueListenable: cheatUpdate,
+                builder: (context, value, widget) {
+                  if (imageFileList.isNotEmpty) {
+                    return SizedBox(
+                        height: 150,
+                        child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: imageFileList.length,
+                            itemBuilder: (context, index) {
+                              return SizedBox(
+                                height: 150,
+                                //Row here in order to add a more controlled version of spacing between the images
+                                child: Row(
+                                  children: [
+                                    Image.file(File(imageFileList[index].path)),
+                                    const SizedBox(
+                                      width: 5,
+                                    ),
+                                  ],
+                                ),
+                              );
+                            }));
+                  } else {
+                    return const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "Images Are Optional",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                      textAlign: TextAlign.center,
-                    ),
-                  );
-                }
-              }
-            ),
+                    );
+                  }
+                }),
             ElevatedButton(
-              onPressed: () => _onPressSubmit(context, imageFileList),
+              onPressed: () =>
+                  _onPressSubmit(context, imageFileList, selectedCondition),
               child: const Text('Submit'),
             ),
           ],
@@ -307,9 +303,18 @@ class _ReportScreenState extends State<ReportScreen> {
   }
 }
 
+// -----------------------------------------------------------------------
+//                                FUNCTIONS
+// -----------------------------------------------------------------------
+
+dismissDialog() {
+  if (dcontext != null) {
+    Navigator.of(dcontext!).pop();
+  }
+}
+
 //Asks user where they want to grab a photo from and calls methods to fetch those photos
 void _onPressUpload(BuildContext context, List<XFile> imageFileList) {
-
   //Set context to pop whenever needed
   dcontext = context;
 
@@ -318,7 +323,7 @@ void _onPressUpload(BuildContext context, List<XFile> imageFileList) {
     child: const Text("Camera"),
   );
   Widget galleryButton = TextButton(
-    onPressed:() => selectImagesGallery(imageFileList),
+    onPressed: () => selectImagesGallery(imageFileList),
     child: const Text("Gallery"),
   );
   Widget cancelButton = TextButton(
@@ -329,24 +334,70 @@ void _onPressUpload(BuildContext context, List<XFile> imageFileList) {
   );
 
   showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: const Text("Choose"),
-      content: const Text("How would you like to upload an image?"),
-      actions: [
-        cameraButton,
-        galleryButton,
-        cancelButton,
-      ],
-    )
-  );
+      context: context,
+      builder: (context) => AlertDialog(
+            title: const Text("Choose"),
+            content: const Text("How would you like to upload an image?"),
+            actions: [
+              cameraButton,
+              galleryButton,
+              cancelButton,
+            ],
+          ));
 }
 
 //Submit the information of the report and leave the report screen
-void _onPressSubmit(BuildContext context, List<XFile> imageFileList) async {
-  imageFileList.clear(); //Needs to be manually cleared
+void _onPressSubmit(BuildContext context, List<XFile> imageFileList,
+    UnsafeCondition? selectedCondition) async {
+  if (selectedCondition == null) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text("Please Select Unsafe Condition Type",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            )),
+      ),
+    );
+    return;
+  }
+
+  //Unless there is change in back end, only first image in list will be submitted.
+  //Kept it this way to show that the code is there to future proof it.
+
+  List<String> encodedImages = [];
+  if (imageFileList.isNotEmpty) {
+    for (var image in imageFileList) {
+      Uint8List bytes = await image.readAsBytes();
+      String convertedImage = base64Encode(bytes);
+      encodedImages.add(convertedImage);
+    }
+  }
+
+  //TODO Implement this with proper URL
+  // Below in the parameters when the code is provided they can be access as such:
+  // widget.coordinates.latitude, widget.coordinates.longitude for coordinate submission
+  // encodedImages[0] for first image as a string, if not empty (will need to see what to pass if image is empty)
+  // selectedCondition.name to get the type
+  // description.text to get the description
+
+  var url = Uri(
+    scheme: 'http',
+    host: appIP,
+    port: appPort,
+    path: '##',
+    queryParameters: {},
+  );
+
+  //Response response = await post(url);
+
+  //Leave Screen Section - Cleanup
+  description.dispose();
   cheatUpdate.value = 0;
-  Navigator.popAndPushNamed(context, '/map');
+
+  if (context.mounted) {
+    context.pop();
+  }
 }
 
 //Get an image from the gallery, can be multiple
@@ -361,11 +412,11 @@ void selectImagesGallery(List<XFile> imageFileList) async {
 
 //Get an image from the camera
 void selectImagesCamera(List<XFile> imageFileList) async {
-  final XFile? selectedImage = await picker.pickImage(source: ImageSource.camera);
+  final XFile? selectedImage =
+      await picker.pickImage(source: ImageSource.camera);
   if (selectedImage != null) {
     imageFileList.add(selectedImage);
   }
   cheatUpdate.value++;
   dismissDialog();
 }
-
