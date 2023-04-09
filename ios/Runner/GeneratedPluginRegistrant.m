c99af69e01be2c474968e8829d12f01f30f2d195
AfnanSa002 <99747471+AfnanSa002@users.noreply.github.com>
2023-04-08 20:45:10 -0400

    OrganizationScreen

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -6,6 +6,12 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<geocoding_ios/GeocodingPlugin.h>)
+#import <geocoding_ios/GeocodingPlugin.h>
+#else
+@import geocoding_ios;
+#endif
+
 #if __has_include(<google_maps_flutter_ios/FLTGoogleMapsPlugin.h>)
 #import <google_maps_flutter_ios/FLTGoogleMapsPlugin.h>
 #else
@@ -27,6 +33,7 @@
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [GeocodingPlugin registerWithRegistrar:[registry registrarForPlugin:@"GeocodingPlugin"]];
   [FLTGoogleMapsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleMapsPlugin"]];
   [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
   [LocationPlugin registerWithRegistrar:[registry registrarForPlugin:@"LocationPlugin"]];
--- lib/screens/view_organization.dart
+++ lib/screens/view_organization.dart
@@ -11,56 +11,106 @@ class  ViewOrganization extends StatefulWidget {
 }
 
 class _ViewOrganizationState extends State<ViewOrganization> {
-  String organizationName = "My Organization";
-  List<String> currentUsers = ["User 1", "User 2", "User 3"];
-  List<String> pendingUsers = ["User 4", "User 5"];
+  final String name = "City of Norfolk";
+  final String phone = "123-456-7890";
+  final String email = "contact@norfolkva.org";
+  final List<String> invitedUsers = [];
+  final TextEditingController _emailController = TextEditingController();
+  String _errorMessage = "";
+
+  void _inviteUser() {
+    String email = _emailController.text.trim();
+    if (email.isEmpty) {
+      return;
+    }
+    if (invitedUsers.contains(email)) {
+      setState(() {
+        _errorMessage = "$email is already invited";
+      });
+    } else {
+      setState(() {
+        invitedUsers.add(email);
+        _errorMessage = "";
+      });
+      _emailController.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('View Organization'),
-        ),
-      body: Column(
+      appBar: AppBar(
+        title: const Text("Organization"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-          Container(
-          padding: const EdgeInsets.all(16.0),
-              child: Text(
-              organizationName,
+            Text(
+              name,
               style: const TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-              ),
-          ),
-          ),
-           Expanded(
-              child: ListView(
-              children: [
-                  ListTile(
-                      title: const Text("Current Users"),
-                      subtitle: Text(currentUsers.join(", ")),
-                  ),
-                  ListTile(
-                      title: const Text("Pending Users"),
-                      subtitle: Text(pendingUsers.join(", ")),
-                  ),
-              ],
-              ),
-            ),
-            Container(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                "${currentUsers.length + pendingUsers.length} / ${currentUsers.length + pendingUsers.length + 10}",
-                style: const TextStyle(
-                fontSize: 16.0,
                 fontWeight: FontWeight.bold,
+                fontSize: 24.0,
+              ),
             ),
+            const SizedBox(height: 16.0),
+            Text(
+              "Phone: $phone",
+              style: const TextStyle(fontSize: 18.0),
             ),
+            const SizedBox(height: 8.0),
+            Text(
+              "Email: $email",
+              style: const TextStyle(fontSize: 18.0),
             ),
-      ]
-
-      )
+            const SizedBox(height: 16.0),
+            const Text(
+              "Invited Users:",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Column(
+              children: invitedUsers
+                  .map(
+                    (user) => ListTile(
+                  title: Text(user),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      setState(() {
+                        invitedUsers.remove(user);
+                      });
+                    },
+                  ),
+                ),
+              )
+                  .toList(),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: "Enter email address to invite",
+                errorText: _errorMessage,
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: _inviteUser,
+                  child: const Text("Invite"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
