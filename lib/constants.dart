619f0eade61c5fcfa14d000db148756ddede36b3
kbank004 <kbank004@odu.edu>
2023-04-12 15:47:00 -0400

    Forgot to include these files in previous commits

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const serverIP = "127.0.0.1";
+  static const serverPort = 8080;
+}
---
+++ lib/features/account/provider.dart
@@ -0,0 +1,4 @@
+class UserState extends StateNot
+
+//old UserState, side menu shows name, login, logout changes it
+// update reportMarkers
