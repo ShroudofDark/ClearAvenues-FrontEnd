adabebe7000d05d8f42f5864cba1f234b231c643
Jacob McFadden <jmcfa001@odu.edu>
2023-04-19 09:27:31 -0400

    Revamping Notification Screen
    Currently in process of rebuilding the notification screen. Right now can basically add any notification to the global "notifications" list. Although only the reminder to report ones have a fully fleshed out display widget currently. The other ones are just text for now. Need to get some sleep, but will finish those off when I continue working on them.

--- lib/features/account/notification_screen.dart
+++ lib/features/account/notification_screen.dart
@@ -1,67 +1,92 @@
+import 'package:clear_avenues/providers.dart';
+import 'package:clear_avenues/models/Notification.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class NotificationScreen extends StatefulWidget {
+class NotificationScreen extends ConsumerStatefulWidget {
   const NotificationScreen({super.key});
-  //TODO
 
   @override
-  State<NotificationScreen> createState() => _NotificationScreenState();
+  ConsumerState<NotificationScreen> createState() => _NotificationScreenState();
 }
 
-class _NotificationScreenState extends State<NotificationScreen> {
-  final List<Map<String, String>> _notifications = [ {"title": "Alert of Nearby Unsafe Condition",
-    "details": "Details about the Alert of Nearby Unsafe Condition"},
-    {"title": "Reminder To Report",
-      "details": "Details about the Reminder To Report"},
-    {"title": "Invitation To Organization",
-      "details": "Details about the Invitation To Organization" },
-    {"title": "Status Update on Report",
-      "details": "Details about the Status Update on Report"},
+class _NotificationScreenState extends ConsumerState<NotificationScreen> {
+  final List<Map<String, String>> _notifications = [
+    {
+      "title": "Alert of Nearby Unsafe Condition",
+      "details": "Details about the Alert of Nearby Unsafe Condition"
+    },
+    {
+      "title": "Reminder To Report",
+      "details": "Details about the Reminder To Report"
+    },
+    {
+      "title": "Invitation To Organization",
+      "details": "Details about the Invitation To Organization"
+    },
+    {
+      "title": "Status Update on Report",
+      "details": "Details about the Status Update on Report"
+    },
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    final user = ref.watch(userProvider);
+    final userName = user.name;
+    if (userName != null) {
+      return Scaffold(
         appBar: AppBar(
           title: const Text("Notifications"),
         ),
-        body: ListView.builder(
-          itemCount: _notifications.length,
-          itemBuilder: (context, index) {
-            return ExpansionTile(
-                title: Text(_notifications[index]["title"]!),
-                children: <Widget>[
-                Container(
-                     padding: const EdgeInsets.all(16.0),
-                     child: Text(_notifications[index]["details"]!),
-            ),
-                Container(
-                     padding: const EdgeInsets.all(16.0),
-                     child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: () {setState(() {
-                            _notifications.removeAt(index);
-                            });
-                          },
-                        ),
-                      ],
-                     ),
-                ),
-            ],
-        );
-        },
+        body: StreamBuilder<List<MyNotification>>(
+          stream: fetchNotifications(),
+          builder: (context, AsyncSnapshot<List<MyNotification>> snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return snapshot.data![index].displayNotification(index, context);
+                },
+              );
+            }
+          }
         ),
-        floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.delete),
-            onPressed: () {
-            setState(() {
-            _notifications.clear();
-            });
-        },
+      //TODO below button is for debugging
+      floatingActionButton: ElevatedButton(
+          child: const Text("Add Notification"),
+          onPressed: () {
+            defaultList.add(exampleNotification);
+          },
         ),
-    );
+      );
+    }
+    //User isn't logged in
+    else {
+      return Scaffold(
+        appBar:  AppBar(
+          title: const Text("Notifications"),
+        ),
+        body: const Center(
+            child: Text("Login to view your Notifications",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+        ),
+      );
+    }
+  }
+
+  Stream<List<MyNotification>> fetchNotifications() async* {
+    while (true) {
+      await Future.delayed(const Duration(seconds: 1), () {});
+      yield defaultList;
+    }
   }
 }
---
+++ lib/models/Notification.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+/// Default Values Stored for Sake of Demo
+MyNotification exampleNotification = MyNotification();
+MyNotification exampleReminderNotification =
+    ReminderNotification(const LatLng(36.886270, -76.309725));
+MyNotification exampleUnsafeNotification = UnsafeNotification();
+MyNotification exampleStatusNotification = StatusNotification();
+
+List<MyNotification> defaultList = [
+  exampleReminderNotification,
+  exampleUnsafeNotification,
+  exampleStatusNotification
+];
+
+void removeNotification(int index) {
+  defaultList.removeAt(index);
+}
+
+class MyNotification {
+  String? title;
+  String? body;
+
+  MyNotification() {
+    title = "Default Type";
+    body = "Default Body";
+  }
+
+  Widget displayNotification(int index, BuildContext context) {
+    return Container(
+      color: Colors.purple[300],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: ExpansionTile(
+          iconColor: Colors.white,
+          textColor: Colors.white,
+          collapsedIconColor: Colors.white,
+          collapsedTextColor: Colors.white,
+          title: Text(
+            title!,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          controlAffinity: ListTileControlAffinity.leading,
+          children: [
+            Text(body!),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ReminderNotification extends MyNotification {
+  LatLng? locationToReport;
+  String address = "Default Location";
+
+  ReminderNotification(LatLng reminderLoc) {
+    locationToReport = reminderLoc;
+    setAddress();
+    title = "Reminder to Report";
+    body = "Click Button to Create Report At:";
+  }
+
+  setAddress() async {
+    List<Placemark> location = await placemarkFromCoordinates(
+        locationToReport!.latitude, locationToReport!.longitude);
+    address =
+        '${location[0].street}, ${location[0].locality}, ${location[0].administrativeArea} ${location[0].postalCode}';
+  }
+
+  @override
+  Widget displayNotification(int index, BuildContext context) {
+    return Container(
+      color: Colors.cyan,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: ExpansionTile(
+          iconColor: Colors.white,
+          textColor: Colors.white,
+          collapsedIconColor: Colors.white,
+          collapsedTextColor: Colors.white,
+          title: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  title!,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    removeNotification(index);
+                  },
+                  child: Row(
+                    children: const [
+                      Icon(Icons.delete),
+                      Text("Delete"),
+                    ],
+                  ))
+            ],
+          ),
+          subtitle: const Text("Received On: Default"),
+          controlAffinity: ListTileControlAffinity.leading,
+          children: [
+            Column(
+              children: [
+                Divider(
+                  thickness: 2,
+                  color: Colors.cyan[300],
+                ),
+                Text(
+                  body!,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.white70,
+                  ),
+                ),
+                Text(
+                  address,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.white70,
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                ElevatedButton(
+                    onPressed: () {
+                      context.push('/report', extra: locationToReport);
+                    },
+                    child: const Text("Create Report")),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class UnsafeNotification extends MyNotification {
+  @override
+  Widget displayNotification(int index, BuildContext context) {
+    return const Text("Default Unsafe Condition");
+  }
+}
+
+class StatusNotification extends MyNotification {
+  @override
+  Widget displayNotification(int index, BuildContext context) {
+    return const Text("Default Status Update");
+  }
+}
