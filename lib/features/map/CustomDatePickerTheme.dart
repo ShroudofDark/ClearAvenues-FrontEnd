2d6bd8b130a9e8748e55c703ef634806d797acb8
kbank004 <kbank004@odu.edu>
2023-04-20 17:14:06 -0400

    Minor optimizations and refactorings. Added location provider. Fixed DatePicker bug where cancel button did not work.

--- lib/features/map/CustomDatePickerTheme.dart
+++ lib/features/map/CustomDatePickerTheme.dart
@@ -1,6 +1,8 @@
 import 'package:clear_avenues/models/Notification.dart';
+import 'package:clear_avenues/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cupertino_datetime_picker/flutter_cupertino_datetime_picker.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:clear_avenues/utility/notification_service.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -8,81 +10,100 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 DateTime selectedTime = DateTime.now();
 
 class MyDateTimePickerTheme extends DateTimePickerTheme {
-
-  MyDateTimePickerTheme(BuildContext context, String userName, LatLng coords):super(
-      titleHeight: 100,
-      title: Container(
-        color: Colors.green,
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+  MyDateTimePickerTheme(BuildContext context, LatLng coords)
+      : super(
+          titleHeight: 100,
+          title: Container(
+            color: Colors.green,
+            child: Column(
               children: [
-                TextButton(
-                  onPressed: () {
-                    context.pop();
-                  },
-                  child: const Text('Cancel',
-                    style: TextStyle(fontSize: 16,
-                      color: Colors.white,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        // Remove both this DatePicker and the AlertDialog from before
+                        context.pop();
+                        context.pop();
+                      },
+                      child: const Text(
+                        'Cancel',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                const Text('Select Reminder Time',
-                  style: TextStyle(fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    String notificationBody = "$userName remember to report! View information under Notifications.";
-                    NotificationService().addNotification(
-                        'Reminder to Report',
-                        notificationBody,
-                        selectedTime.millisecondsSinceEpoch,
-                        'ReportLater',
-                    );
-                    int secondsToNotify =
-                      ((selectedTime.millisecondsSinceEpoch - DateTime.now().millisecondsSinceEpoch)/1000).round();
-                    createReminderNotificationDelayed(coords, secondsToNotify);
-                    context.pop();
-                  },
-                  child: const Text('Done',
-                    style: TextStyle(fontSize: 16,
-                      color: Colors.white,
+                    const Text(
+                      'Select Reminder Time',
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
-                  ),
+                    Consumer(builder: (context, ref, child) {
+                      return TextButton(
+                        onPressed: () {
+                          String notificationBody =
+                              "${ref.watch(userProvider).name} remember to report! View information under Notifications.";
+                          NotificationService().addNotification(
+                            'Reminder to Report',
+                            notificationBody,
+                            selectedTime.millisecondsSinceEpoch,
+                            'ReportLater',
+                          );
+                          int secondsToNotify = ((selectedTime
+                                          .millisecondsSinceEpoch -
+                                      DateTime.now().millisecondsSinceEpoch) /
+                                  1000)
+                              .round();
+                          createReminderNotificationDelayed(
+                              coords, secondsToNotify);
+                          context.pop();
+                        },
+                        child: const Text(
+                          'Done',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      );
+                    }),
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    Text(
+                      'Day',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    Text(
+                      'Hour',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    Text('Minute',
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white)),
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
                 ),
               ],
             ),
-            const Padding( padding: EdgeInsets.symmetric(vertical: 8.0),),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                Text('Day',
-                  style: TextStyle(fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white
-                  ),
-                ),
-                Text('Hour',
-                  style: TextStyle(fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white
-                  ),
-                ),
-                Text('Minute',
-                    style: TextStyle(fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white)
-                ),
-              ],
-            ),
-            const Padding( padding: EdgeInsets.symmetric(vertical: 8.0),),
-          ],
-        ),
-      ),
-
-  );
+          ),
+        );
 }
---
+++ lib/features/map/report_method_dialog.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cupertino_datetime_picker/flutter_cupertino_datetime_picker.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'CustomDatePickerTheme.dart';
+
+class ReportMethodDialog extends StatelessWidget {
+  const ReportMethodDialog( this.coords, {Key? key}) : super(key: key);
+  final LatLng coords;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("Choose"),
+      content: const Text("Would you like to submit a report now?"),
+      actions: [
+        TextButton(
+          onPressed: () {
+            context.pop();
+            context.push("/report", extra: coords);
+          },
+          child: const Text("Submit"),
+        ),
+        TextButton(
+          onPressed: () {
+            /*
+       * Likely in hindsight a better method of doing this would
+       * be to just ask for "hours from now", pick from drop down, and so
+       * forth. -Jacob
+       */
+            //Limits user from picking time before current time
+            DateTime dateLimitMin =
+                DateTime.now().copyWith(minute: DateTime.now().minute + 1);
+            //Limit user from picking something so far out that it makes no sense
+            DateTime dateLimitMax =
+                DateTime.now().copyWith(day: DateTime.now().day + 5);
+            //Set global variable for CustomDatePicker
+            selectedTime = dateLimitMin;
+            DatePicker.showDatePicker(context,
+                dateFormat: 'dd HH:mm',
+                initialDateTime: dateLimitMin,
+                minDateTime: dateLimitMin,
+                maxDateTime: dateLimitMax,
+                onMonthChangeStartWithFirstDate: true,
+                onChange: (dateTime, List<int> index) {selectedTime = dateTime;},
+                pickerTheme:
+                    MyDateTimePickerTheme(context, coords));
+            },
+          child: const Text("Remind Me Later"),
+        ),
+        TextButton(
+          onPressed: () => context.pop(),
+          child: const Text("Close"),
+        )
+      ],
+    );
+  }
+}
