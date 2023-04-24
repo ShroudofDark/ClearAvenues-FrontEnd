453d88a032a75673052cb9dd6497b5a445653b8b
Jacob McFadden <jmcfa001@odu.edu>
2023-04-24 07:19:46 -0400

    More Account Info
    Setup for more functions, need back end functions to change an existing user's email and password

--- lib/features/account/account_info.dart
+++ lib/features/account/account_info.dart
@@ -11,9 +11,11 @@ class AccountInfoScreen extends ConsumerStatefulWidget {
 }
 
 class _AccountInfoScreen extends ConsumerState<AccountInfoScreen> {
-
   TextEditingController emailControl = TextEditingController();
   TextEditingController passwordControl = TextEditingController();
+
+  final emailKey = GlobalKey<FormState>();
+  final passwordKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,75 +24,137 @@ class _AccountInfoScreen extends ConsumerState<AccountInfoScreen> {
       appBar: AppBar(
         title: const Text('Account Information'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Email Address',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16.0,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Account Name',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
               ),
-            ),
-            Text(
-              "${user.email}",
-              style: const TextStyle(
-                fontSize: 16.0,
+              Text(
+                "${user.name}",
+                style: const TextStyle(
+                  fontSize: 16.0,
+                ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            const Text(
-              'Account Type',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16.0,
+              const SizedBox(height: 16.0),
+              const Text(
+                'Email Address',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
               ),
-            ),
-            Text(
-              "${user.accountType}",
-              style: const TextStyle(
-                fontSize: 16.0,
+              Text(
+                "${user.email}",
+                style: const TextStyle(
+                  fontSize: 16.0,
+                ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            const Text(
-              'Change Email Address',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16.0,
+              const SizedBox(height: 16.0),
+              const Text(
+                'Account Type',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
               ),
-            ),
-            TextField(
-              controller: emailControl,
-              decoration: const InputDecoration(
-                hintText: 'New Email Address',
+              Text(
+                "${user.accountType}",
+                style: const TextStyle(
+                  fontSize: 16.0,
+                ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            const Text(
-              'Change Password',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16.0,
+              const SizedBox(height: 16.0),
+              const Text(
+                'Change Email Address',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
               ),
-            ),
-            TextField(
-              controller: passwordControl,
-              decoration: const InputDecoration(
-                hintText: 'New Password',
+              Form(
+                key: emailKey,
+                child: TextFormField(
+                  controller: emailControl,
+                  decoration: const InputDecoration(
+                    hintText: 'New Email Address',
+                  ),
+                  validator: (value) {
+                    if(value == null || value.isEmpty) {
+                      return 'Cannot Be Empty';
+                    }
+                    return null;
+                  },
+                ),
               ),
-            ),
-            const SizedBox(height: 32.0),
-            ElevatedButton(
-              onPressed: () {
-                ref.read(userProvider.notifier).logout();
-                context.pop();
-              },
-              child: const Text('Logout'),
-            ),
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  if(emailKey.currentState!.validate()) {
+                    debugPrint("Temp");
+                    emailControl.clear();
+                  }
+                },
+                child: const Text(
+                  "Submit",
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              const Text(
+                'Change Password',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
+              ),
+              Form(
+                key: passwordKey,
+                child: TextFormField(
+                  controller: passwordControl,
+                  decoration: const InputDecoration(
+                    hintText: 'New Password',
+                  ),
+                  validator: (value) {
+                    if(value == null || value.isEmpty) {
+                      return 'Cannot Be Empty';
+                    }
+                    if (passwordControl.text.length < 8) {
+                      return 'Minimum of 8 Characters Required';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if(passwordKey.currentState!.validate()) {
+                    debugPrint("Temp");
+                    passwordControl.clear();
+                  }
+                },
+                child: const Text(
+                  "Submit",
+                ),
+              ),
+              const SizedBox(height: 32.0),
+              Container(
+                alignment: Alignment.bottomCenter,
+                child: ElevatedButton(
+                  onPressed: () {
+                    ref.read(userProvider.notifier).logout();
+                    context.pop();
+                  },
+                  child: const Text('Logout'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
